9c86fd1f16f7ae065a32a58395234f9363c66903
Jan Hülsmann <mail@janhuelsmann.com>
2022-04-09 01:10:16 +0200

    🎨 Icons for Safari and Android

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,11 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/img/*");
 	eleventyConfig.addPassthroughCopy("src/robots.txt");
 	eleventyConfig.addPassthroughCopy("src/favicon.svg");
+	eleventyConfig.addPassthroughCopy("src/favicon.ico");
+	eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+	eleventyConfig.addPassthroughCopy("src/manifest.webmanifest");
+	eleventyConfig.addPassthroughCopy("src/icon-192.png");
+	eleventyConfig.addPassthroughCopy("src/icon-512.png");
 	
 	eleventyConfig.setBrowserSyncConfig({
 		middleware: [
